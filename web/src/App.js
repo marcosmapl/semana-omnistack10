763f424362ced3527f83dca53e6e6ddbea959c72
marcos.lima@icomp.ufam.edu.br <marcos.lima@icomp.ufam.edu.br>
2020-01-15 13:17:05 -0400

    Create Front End Web Project

---
+++ web/src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+/*
+  Conceitos principais do React
+    * Componente: Função que retorna algum tipo de conteúdo (HTML, CSS, JS)
+    * Estado
+    * Propriedade
+*/
+function App() {
+  return (
+    <h1>Hello World!</h1>  
+  );
+}
+
+export default App;
