cd9be0c39530793a3220bd8985573e76a0dc17a0
marcos.lima@icomp.ufam.edu.br <marcos.lima@icomp.ufam.edu.br>
2020-01-17 17:19:00 -0400

    Create Main & Profile Pages

--- mobile/App.js
+++ mobile/App.js
@@ -1,26 +1,8 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import Routes from './src/routes';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text style={styles.title}>Hello OmniStack Week 10!</Text>
-    </View>
+    <Routes />
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#7159c1',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-  title: {
-    fontWeight: 'bold',
-    fontSize: 26,
-    color: '#FFF',
-  },
-});
-
-//https://github.com/Rocketseat/expo-common-issues  
