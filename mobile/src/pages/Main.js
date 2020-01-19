f6c6c1cb9ffd7396ff20ebf9e6c17c50e3270b33
marcos.lima@icomp.ufam.edu.br <marcos.lima@icomp.ufam.edu.br>
2020-01-19 09:46:51 -0400

    Add Webview in Profile Page

--- mobile/src/pages/Main.js
+++ mobile/src/pages/Main.js
@@ -1,9 +1,9 @@
 import React, { useEffect, useState } from 'react';
-import { StyleSheet, View, Dimensions, Image } from 'react-native';
-import MapView, { Marker } from 'react-native-maps';
+import { StyleSheet, View, Text, Dimensions, Image } from 'react-native';
+import MapView, { Marker, Callout } from 'react-native-maps';
 import { requestPermissionsAsync, getCurrentPositionAsync } from 'expo-location';
 
-function Main() {
+function Main({ navigation }) {
   const [currentRegion, setCurrentRegion] = useState(null);
 
   useEffect(() => {
@@ -36,8 +36,18 @@ function Main() {
   return (
     <View style={styles.container}>
       <MapView initialRegion={currentRegion} style={styles.mapStyle} >
-        <Marker coordinate={{latitude: -3.1102162, longitude: -59.9852251 }}>
-          <Image style={styles.avatar} source={{ uri:'https://www.sideshow.com/storage/product-images/903188/bb-8_star-wars_silo.png'}} />
+        <Marker coordinate={{ latitude: -3.1102162, longitude: -59.9852251 }}>
+          <Image style={styles.avatar} source={{ uri: 'https://www.sideshow.com/storage/product-images/903188/bb-8_star-wars_silo.png' }} />
+
+          <Callout onPress={() => {
+            navigation.navigate('Profile', { github_username: 'marcosmapl' });
+          }}>
+            <View style={styles.callout}>
+              <Text style={styles.developerName}>Marcos Lima</Text>
+              <Text style={styles.developerBio}>Computer Science Student ar Federal University of Amazonas</Text>
+              <Text style={styles.developerTechs}>Python, Java, C, SQL</Text>
+            </View>
+          </Callout>
         </Marker>
       </MapView>
     </View>
@@ -51,15 +61,36 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+
   mapStyle: {
     width: Dimensions.get('window').width,
     height: Dimensions.get('window').height,
   },
+
   avatar: {
     width: 54,
     height: 54,
     borderRadius: 4,
+    borderWidth: 4,
     borderColor: '#FFF',
+  },
+
+  callout: {
+    width: 260,
+  },
+
+  developerName: {
+    fontWeight: 'bold',
+    fontSize: 16,
+  },
+
+  developerBio: {
+    color: '#666',
+    marginTop: 5,
+  },
+
+  developerTechs: {
+    marginTop: 5,
   },
 });
 
